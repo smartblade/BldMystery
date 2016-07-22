48d1ff2a3d8dd143e494861e0d61316dbbc1ea3c
Blade Smart <smartblader@gmail.com>
2016-07-22 14:15:14 +0500

    NetBlade: create Borland C wrapper for bld_net_cb

--- NetBlade/bcb_wrappers.cpp
+++ NetBlade/bcb_wrappers.cpp
@@ -13,7 +13,7 @@ LIB_EXP bld_abstract_net *bcb_GetNetInterface(bld_net_cb *cb, void *module);
 #endif
 
 
-#define DEFINE_VC2BCB_METHOD_WRAPPER(offset, argsize) {\
+#define DEFINE_VC2BC_METHOD_WRAPPER(offset, argsize) {\
         _asm { mov eax, [ecx + 0x04] }\
         _asm { mov edx, [eax] }\
         _asm { sub esp, argsize }\
@@ -28,18 +28,32 @@ LIB_EXP bld_abstract_net *bcb_GetNetInterface(bld_net_cb *cb, void *module);
         _asm { ret argsize}\
 }
 
-#define DEFINE_VC2BCB_METHOD_WRAPPER_RET(offset, argsize, ret_value) {\
-        DEFINE_VC2BCB_METHOD_WRAPPER(offset, argsize)\
+#define DEFINE_VC2BC_METHOD_WRAPPER_RET(offset, argsize, ret_value) {\
+        DEFINE_VC2BC_METHOD_WRAPPER(offset, argsize)\
         return ret_value;\
 }
 
+#define DEFINE_BC2VC_METHOD_WRAPPER(offset, argsize) {\
+        _asm { mov ecx, [esp + 0x04] }\
+        _asm { mov eax, [ecx + 0x04] }\
+        _asm { mov edx, [eax] }\
+        _asm { sub esp, argsize }\
+        _asm { mov edi, esp }\
+        _asm { lea esi, esp + 0x08 + argsize }\
+        _asm { mov ecx, argsize }\
+        _asm { rep movsb }\
+        _asm { mov ecx, eax }\
+        _asm { call [edx + offset] }\
+        _asm { ret }\
+}
 
-class vc2bcb_net : public bld_abstract_net
+
+class vc2bc_net : public bld_abstract_net
 {
 private:
         bld_abstract_net *net;
 public:
-        vc2bcb_net(bld_abstract_net *net)
+        vc2bc_net(bld_abstract_net *net)
         {
                 this->net = net;
         }
@@ -63,50 +77,89 @@ public:
         virtual bool is_valid_protocol(bool tcp);
 };
 
-__declspec(naked) unsigned long vc2bcb_net::get_session_player_dpid()
-        DEFINE_VC2BCB_METHOD_WRAPPER_RET(0x00, 0x00, false)
+__declspec(naked) unsigned long vc2bc_net::get_session_player_dpid()
+        DEFINE_VC2BC_METHOD_WRAPPER_RET(0x00, 0x00, false)
 
-__declspec(naked) unsigned long vc2bcb_net::get_all_players_id()
-        DEFINE_VC2BCB_METHOD_WRAPPER_RET(0x04, 0x00, false)
+__declspec(naked) unsigned long vc2bc_net::get_all_players_id()
+        DEFINE_VC2BC_METHOD_WRAPPER_RET(0x04, 0x00, false)
 
-__declspec(naked) bool vc2bcb_net::is_net_game()
-        DEFINE_VC2BCB_METHOD_WRAPPER_RET(0x08, 0x00, false)
+__declspec(naked) bool vc2bc_net::is_net_game()
+        DEFINE_VC2BC_METHOD_WRAPPER_RET(0x08, 0x00, false)
 
-__declspec(naked) bool vc2bcb_net::is_server()
-        DEFINE_VC2BCB_METHOD_WRAPPER_RET(0x0C, 0x00, false)
+__declspec(naked) bool vc2bc_net::is_server()
+        DEFINE_VC2BC_METHOD_WRAPPER_RET(0x0C, 0x00, false)
 
-__declspec(naked) void vc2bcb_net::unknown010()
-        DEFINE_VC2BCB_METHOD_WRAPPER_RET(0x10, 0x00, (void)false)
+__declspec(naked) void vc2bc_net::unknown010()
+        DEFINE_VC2BC_METHOD_WRAPPER_RET(0x10, 0x00, (void)false)
 
-__declspec(naked) long vc2bcb_net::send_message(
+__declspec(naked) long vc2bc_net::send_message(
         unsigned long idTo, void *lpData, unsigned long wDataSize)
-        DEFINE_VC2BCB_METHOD_WRAPPER_RET(0x14, 0x0C, 0)
+        DEFINE_VC2BC_METHOD_WRAPPER_RET(0x14, 0x0C, 0)
 
-__declspec(naked) long vc2bcb_net::send_guaranteed_message(
+__declspec(naked) long vc2bc_net::send_guaranteed_message(
                 unsigned long idTo, void *lpData, unsigned long wDataSize
         )
-        DEFINE_VC2BCB_METHOD_WRAPPER_RET(0x18, 0x0C, 0)
+        DEFINE_VC2BC_METHOD_WRAPPER_RET(0x18, 0x0C, 0)
 
-__declspec(naked) bool vc2bcb_net::start_server(
+__declspec(naked) bool vc2bc_net::start_server(
         const char *game_name, const char *player_name, int max_players,
         bool TCP
 )
-        DEFINE_VC2BCB_METHOD_WRAPPER_RET(0x1C, 0x10, false)
+        DEFINE_VC2BC_METHOD_WRAPPER_RET(0x1C, 0x10, false)
 
-__declspec(naked) bool vc2bcb_net::browse_sessions(const char *ip_address)
-        DEFINE_VC2BCB_METHOD_WRAPPER_RET(0x20, 0x04, false)
+__declspec(naked) bool vc2bc_net::browse_sessions(const char *ip_address)
+        DEFINE_VC2BC_METHOD_WRAPPER_RET(0x20, 0x04, false)
 
-__declspec(naked) bool vc2bcb_net::get_browse_result(int index, bld_server_info *info)
-        DEFINE_VC2BCB_METHOD_WRAPPER_RET(0x24, 0x08, false)
+__declspec(naked) bool vc2bc_net::get_browse_result(int index, bld_server_info *info)
+        DEFINE_VC2BC_METHOD_WRAPPER_RET(0x24, 0x08, false)
 
-__declspec(naked) bool vc2bcb_net::join_session(int index, const char *player_name)
-        DEFINE_VC2BCB_METHOD_WRAPPER_RET(0x28, 0x08, false)
+__declspec(naked) bool vc2bc_net::join_session(int index, const char *player_name)
+        DEFINE_VC2BC_METHOD_WRAPPER_RET(0x28, 0x08, false)
 
-__declspec(naked) bool vc2bcb_net::is_valid_protocol(bool tcp)
-        DEFINE_VC2BCB_METHOD_WRAPPER_RET(0x2C, 0x04, false)
+__declspec(naked) bool vc2bc_net::is_valid_protocol(bool tcp)
+        DEFINE_VC2BC_METHOD_WRAPPER_RET(0x2C, 0x04, false)
+
+
+class bc2vc_net_cb : public bld_net_cb
+{
+private:
+        bld_net_cb *cb;
+public:
+        bc2vc_net_cb(bld_net_cb *cb)
+        {
+                this->cb = cb;
+        }
+        virtual void message_received(
+                unsigned long idFrom, void *message, unsigned long messageSize
+        );
+        virtual void player_destroyed(unsigned long playerId);
+        virtual void player_created(
+                unsigned long playerId, const char *name
+        );
+        virtual void unknown00C(int i1, int i2, int i3, const char *s);
+};
+
+__declspec(naked) void bc2vc_net_cb::message_received(
+        unsigned long idFrom, void *message, unsigned long messageSize
+)
+        DEFINE_BC2VC_METHOD_WRAPPER(0x00, 0x0C)
+
+__declspec(naked) void bc2vc_net_cb::player_destroyed(unsigned long playerId)
+        DEFINE_BC2VC_METHOD_WRAPPER(0x04, 0x04)
+
+__declspec(naked) void bc2vc_net_cb::player_created(
+        unsigned long playerId, const char *name
+)
+        DEFINE_BC2VC_METHOD_WRAPPER(0x08, 0x08)
+
+__declspec(naked) void bc2vc_net_cb::unknown00C(
+        int i1, int i2, int i3, const char *s
+)
+        DEFINE_BC2VC_METHOD_WRAPPER(0x0C, 0x10)
 
 
 bld_abstract_net *bcb_GetNetInterface(bld_net_cb *cb, void *module) {
-        bld_abstract_net *net = GetNetInterface(cb, module);
-        return new vc2bcb_net(net);
+        bc2vc_net_cb *bc2vc_cb = new bc2vc_net_cb(cb);
+        bld_abstract_net *net = GetNetInterface(bc2vc_cb, module);
+        return new vc2bc_net(net);
 }
