3ed1827f1e5aeb3fcb024cbfb84ac3a623823a27
Blade Smart <smartblader@gmail.com>
2020-01-04 19:09:35 +0500

    Add test to verify ABI of B_CameraEntity class

--- tests/UnitTests/Blade/CameraEntityTests.cpp
+++ tests/UnitTests/Blade/CameraEntityTests.cpp
@@ -1,4 +1,5 @@
 #include <gtest/gtest.h>
+#include <Utils/FieldOffset.h>
 #include <bld_system.h>
 #include "entity.h"
 
@@ -6,4 +7,10 @@
 TEST(CameraEntityTests, SizeOf)
 {
     EXPECT_EQ(sizeof(B_CameraEntity), 0x43C);
+}
+
+TEST(CameraEntityTests, Fields)
+{
+    EXPECT_OFFSET_EQ(B_CameraEntity, unknownValueFromApplication, 0x3EC);
+    EXPECT_OFFSET_EQ(B_CameraEntity, unknownPtrFromApplication, 0x3F0);
 }
