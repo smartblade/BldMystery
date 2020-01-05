264c1db5a3c81cecddfb563746bd3dedbe9a6f15
Blade Smart <smartblader@gmail.com>
2020-01-05 12:03:11 +0500

    Add tests to verify ABI of CombustionTests class

---
+++ tests/UnitTests/Blade/CombustionTests.cpp
@@ -0,0 +1,17 @@
+#include <gtest/gtest.h>
+#include <Utils/FieldOffset.h>
+#include <bld_system.h>
+#define private public
+#include "bld_misc_funcs.h"
+
+
+TEST(CombustionTests, SizeOf)
+{
+    EXPECT_EQ(sizeof(B_Combustion), 0x5C);
+}
+
+TEST(CombustionTests, Fields)
+{
+    EXPECT_OFFSET_EQ(B_Combustion, a00C, 0x00C);
+    EXPECT_OFFSET_EQ(B_Combustion, unknown0024, 0x024);
+}
