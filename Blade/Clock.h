48c43390dd114ca3940b4b5d4fcd0882b3aecae7
Blade Smart <smartblader@gmail.com>
2020-02-15 15:35:20 +0500

    Fix indentation of Clock.h

--- Blade/Clock.h
+++ Blade/Clock.h
@@ -7,21 +7,21 @@
 class B_Clock
 {
 public:
-        virtual void unknown000();
-        virtual ~B_Clock();
-        virtual void SetTimeSpeed(double speed);
-        virtual double GetTimeSpeed();
-        virtual double GetTime();
-        virtual void SetTime(double time);
-        virtual void unknown018();
-        virtual void StopTime();
-        virtual void RestartTime();
+    virtual void unknown000();
+    virtual ~B_Clock();
+    virtual void SetTimeSpeed(double speed);
+    virtual double GetTimeSpeed();
+    virtual double GetTime();
+    virtual void SetTime(double time);
+    virtual void unknown018();
+    virtual void StopTime();
+    virtual void RestartTime();
 
-        boolean bUnknown04;
-        double fUnknown08;
-        double fUnknown10;
-        double fUnknown18;
-        char unknownFields2[36];
+    boolean bUnknown04;
+    double fUnknown08;
+    double fUnknown10;
+    double fUnknown18;
+    char unknownFields2[36];
 };
 
 #endif /* B_CLOCK_H */
