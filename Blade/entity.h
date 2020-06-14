3adb3b901d4898262ad234eab9c4195f1988b179
Blade Smart <smartblader@gmail.com>
2020-06-14 14:40:58 +0500

    Add more macro definitions for entity kinds

--- Blade/entity.h
+++ Blade/entity.h
@@ -8,7 +8,12 @@
 
 #define PLAYER "Player1"
 
+#define B_ENTITY_CID_PHYSIC_OBJECT     2
 #define B_ENTITY_CID_PERSON            4
+#define B_ENTITY_CID_WEAPON            6
+#define B_ENTITY_CID_ACTOR             13
+#define B_ENTITY_CID_BIPED             21
+#define B_ENTITY_CID_CLIENT            27
 
 class B_Entity :  public B_NamedObj
 {
