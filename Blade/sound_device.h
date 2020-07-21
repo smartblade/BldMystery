3d56c9d108b00ac851c8f6bc797bbd2e5fd34ea2
Blade Smart <smartblader@gmail.com>
2020-07-21 22:47:28 +0500

    Fix indentation of sound_device.h

--- Blade/sound_device.h
+++ Blade/sound_device.h
@@ -9,36 +9,36 @@ class B_Matrix;
 class sound_t
 {
 public:
-        sound_t();
-        virtual void unknown000();
-        virtual ~sound_t();
-        virtual void unknown008();
-        virtual void unknown00C();
-        virtual void unknown010();
-        virtual int init();
-        virtual void unknown018();
-        virtual bool SetProviderId(int id);
-        virtual void unknown020();
-        virtual void unknown024();
-        virtual void unknown028();
-        virtual void unknown02C();
-        virtual bool set_world_size(
-                double a, double b, double x, double d, double e);
-        virtual void unknown034();
-        virtual void unknown038();
-        virtual void unknown03C();
-        virtual void unknown040();
-        virtual void unknown044();
-        virtual void unknown048();
-        virtual void unknown04C();
-        virtual void unknown050();
-        virtual void sound_unknown054();
-        virtual void SetListenerPosition(B_Matrix &pos);
+    sound_t();
+    virtual void unknown000();
+    virtual ~sound_t();
+    virtual void unknown008();
+    virtual void unknown00C();
+    virtual void unknown010();
+    virtual int init();
+    virtual void unknown018();
+    virtual bool SetProviderId(int id);
+    virtual void unknown020();
+    virtual void unknown024();
+    virtual void unknown028();
+    virtual void unknown02C();
+    virtual bool set_world_size(
+        double a, double b, double x, double d, double e);
+    virtual void unknown034();
+    virtual void unknown038();
+    virtual void unknown03C();
+    virtual void unknown040();
+    virtual void unknown044();
+    virtual void unknown048();
+    virtual void unknown04C();
+    virtual void unknown050();
+    virtual void sound_unknown054();
+    virtual void SetListenerPosition(B_Matrix &pos);
 
-        int unknown04;
-        int unknown08;
-        int unknown0C;
-        char unknownFields10[596];
+    int unknown04;
+    int unknown08;
+    int unknown0C;
+    char unknownFields10[596];
 };
 
 #endif
