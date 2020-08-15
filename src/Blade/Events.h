e340c314c31c7550b7a0e91bee99be9827cb8461
Blade Smart <smartblader@gmail.com>
2020-08-15 18:08:51 +0500

    Blade: reconstruct B_Events::GetEventName

--- src/Blade/Events.h
+++ src/Blade/Events.h
@@ -14,6 +14,9 @@ class B_Events
 {
 public:
     int GetEventIndex(const char *event_name);
+    const B_Name &GetEventName(int eventIndex);
+private:
+    B_PtrArray<B_Event> events;
 };
 
 #endif /* EVENTS_H */
---
+++ tests/UnitTests/Blade/EventsTests.cpp
@@ -0,0 +1,13 @@
+#include <gtest/gtest.h>
+#include <Utils/FieldOffset.h>
+#include <bld_system.h>
+#define private public
+#include "Events.h"
+
+
+TEST(EventsTests, Fields)
+{
+    EXPECT_OFFSET_EQ(B_Events, events, 0x0000);
+    EXPECT_OFFSET_EQ(B_Events, events.elements, 0x0004);
+    EXPECT_OFFSET_EQ(B_Events, events.size, 0x0008);
+}
