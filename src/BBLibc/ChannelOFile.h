c434e5882c0f2369371fa859c2bca9598971ec0e
Blade Smart <smartblader@gmail.com>
2025-12-18 20:33:10 +0500

    Add placeholders for BBLibc inline methods to header files

--- src/BBLibc/ChannelOFile.h
+++ src/BBLibc/ChannelOFile.h
@@ -17,4 +17,31 @@ class B_ChannelOFile : public B_MessageChannel
     virtual unsigned int Open();
     virtual unsigned int Close();
     virtual unsigned int Message(const char *arg_1);
+
+    /*
+    * Module:                 BBLibc.dll
+    * Entry point:            0x10029A60
+    * VC++ mangling:          ??0B_ChannelOFile@@QAE@ABV0@@Z
+    */
+#ifndef BLD_NATIVE
+    B_ChannelOFile(const B_ChannelOFile &arg_1)
+    {
+    }
+#else
+    B_ChannelOFile(const B_ChannelOFile &arg_1);
+#endif
+
+    /*
+    * Module:                 BBLibc.dll
+    * Entry point:            0x10029AC0
+    * VC++ mangling:          ??4B_ChannelOFile@@QAEAAV0@ABV0@@Z
+    */
+#ifndef BLD_NATIVE
+    B_ChannelOFile &operator =(const B_ChannelOFile &arg_1)
+    {
+        return *this;
+    }
+#else
+    B_ChannelOFile &operator =(const B_ChannelOFile &arg_1);
+#endif
 };
